b11fa4cfd9ccd8cf965257aadb31fdab57d92902
Shruti Priya <shrutipriya1808@gmail.com>
2017-03-03 14:26:18 -0800

    add routing to show list of venues and add a new venue

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -1,15 +1,34 @@
 using Nancy;
 using System;
 using System.Collections.Generic;
+using Nancy.ViewEngines.Razor;
 
 
 namespace BandTracker
 {
-  public class HomeModule : NancyModule
-  {
-    public HomeModule()
+    public class HomeModule : NancyModule
     {
-      Get["/"] = _ =>
-      {
-        return View["index.cshtml"];
-      };
+        public HomeModule()
+        {
+            Get["/"] = _ =>
+            {
+                return View["index.cshtml"];
+            };
+
+            Get["/venues"] = _ => {
+                List<Venue> allVenues = Venue.GetAll();
+                return View["venues.cshtml", allVenues];
+            };
+
+            Get["/venues/new"] = _ => {
+                return View["venues_form.cshtml"];
+            };
+
+            Post["/venues/new"] = _ => {
+                Venue newVenue = new Venue(Request.Form["venue-name"]);
+                newVenue.Save();
+                return View["success.cshtml"];
+            };
+        }
+    }
+}
