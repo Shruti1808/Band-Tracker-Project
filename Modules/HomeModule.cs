8aad42a39ab4ac6d65ef87da7600abcf078a2bc0
Shruti Priya <shrutipriya1808@gmail.com>
2017-03-03 14:41:14 -0800

    add page to edit and view the venue

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -29,6 +29,17 @@ namespace BandTracker
                 newVenue.Save();
                 return View["success.cshtml"];
             };
+
+            Get["venue/edit/{id}"] = parameters => {
+                Venue SelectedVenue = Venue.Find(parameters.id);
+                return View["venue_edit.cshtml", SelectedVenue];
+            };
+
+            Patch["venue/edit/{id}"] = parameters => {
+                Venue SelectedVenue = Venue.Find(parameters.id);
+                SelectedVenue.Update(Request.Form["venue-name"]);
+                return View["success.cshtml"];
+            };
         }
     }
 }
